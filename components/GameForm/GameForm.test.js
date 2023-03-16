93849867db646411bae0e9a8d2933d59fda573ab
Anja Siewert <anja.siewert@live.de>
2023-03-16 21:37:57 +0100

    fix input test function

--- components/GameForm/GameForm.test.js
+++ components/GameForm/GameForm.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+//import { input } from "@testing-library/user-event/dist/types/event";
 import GameForm from "./index";
 
 jest.mock("next/router", () => ({
@@ -8,7 +9,13 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders two input fields and a button", () => {});
+test("renders two input fields and a button", () => {
+  render(<GameForm />);
+  const inputs = screen.getAllByPlaceholderText(/e.g./i);
+  const button = screen.getByRole("button", { name: /create game/i });
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
 
 test("renders a form with the accessible name 'Create a new game'", () => {});
 
--- components/Input/Input.test.js
+++ components/Input/Input.test.js
@@ -3,23 +3,16 @@ import userEvent from "@testing-library/user-event";
 import Input from ".";
 
 test("renders a label and an input with the correct attributes", () => {
-  render(
-    <>
-      <Input name="game" labelText="labelText" placeholder="placeholder" />
-      {/* <label htmlFor="for" labelText="label" /> */}
-    </>
-  );
+  render(<Input name="game" labelText="labelText" placeholder="placeholder" />);
   const input = screen.getByLabelText("labelText");
   expect(input).toHaveAttribute("placeholder", "placeholder");
-  /* const label = screen.getByLabelText("label");
-  expect(label).toHaveAttribute("htmlFor", "for"); */
 });
 
-/* test("calls callback on every user input", async () => {
+test("calls callback on every user input", async () => {
   const onChange = jest.fn();
   const user = userEvent.setup();
-  render(<Input onChange={onChange} labelText="input field" />);
+  render(<Input onChange={onChange} labelText="input field" name="input" />);
   const input = screen.getByLabelText("input field");
   await user.type(input, "abc");
-  expect(onChange).toHaveBeenCalledTimes(1);
-}); */
+  expect(onChange).toHaveBeenCalledTimes(3);
+});
