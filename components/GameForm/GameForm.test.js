f87ec74b2d468ae0cd9a8d8d364793d878939760
Anja Siewert <anja.siewert@live.de>
2023-03-16 21:58:42 +0100

    add submit form test

--- components/GameForm/GameForm.test.js
+++ components/GameForm/GameForm.test.js
@@ -17,8 +17,26 @@ test("renders two input fields and a button", () => {
   expect(button).toBeInTheDocument();
 });
 
-test("renders a form with the accessible name 'Create a new game'", () => {});
+test("renders a form with the accessible name 'Create a new game'", () => {
+  render(<GameForm />);
+  const form = screen.getByText(/create a new game/i);
+  expect(form).toBeInTheDocument();
+});
 
-test("submits the correct form data when every field is filled out", async () => {});
+test("submits the correct form data when every field is filled out", async () => {
+  const handleSubmit = jest.fn();
+  const user = userEvent.setup();
+  render(<GameForm onCreateGame={handleSubmit} />);
+  const name = screen.getByLabelText(/name of game/i);
+  const player = screen.getByLabelText(/player names/i);
+  const button = screen.getByRole("button", { name: "Create game" });
+  await user.type(name, "Uno");
+  await user.type(player, "Anja");
+  await user.click(button);
+  expect(handleSubmit).toHaveBeenCalledWith({
+    nameOfGame: "Uno",
+    playerNames: ["Anja"],
+  });
+});
 
 test("does not submit form if one input field is left empty", async () => {});
