60b04e1fe217bbd91d9288017d0543b00dde0db7
Anja Siewert <anja.siewert@live.de>
2023-03-16 18:25:24 +0100

    add input test

--- components/Input/Input.test.js
+++ components/Input/Input.test.js
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input />);
+  const input = screen.getByRole("textbox", { name: "" });
+  expect(input).toHaveAttribute("type", "text");
+  /* const label = screen.getByLabelText("");
+  expect(label).toHaveAttribute("for", ""); */
+});
 
 test("calls callback on every user input", async () => {});
